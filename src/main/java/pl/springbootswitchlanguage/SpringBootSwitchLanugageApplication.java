df61ccd2bd78246b907839a7c5b83c49407f29ea
kwasc <kwasc@LAPTOP-2JHTQSFL>
2020-10-03 20:34:53 +0200

    v.1

---
+++ src/main/java/pl/springbootswitchlanguage/SpringBootSwitchLanugageApplication.java
@@ -0,0 +1,13 @@
+package pl.springbootswitchlanguage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSwitchLanugageApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootSwitchLanugageApplication.class, args);
+  }
+
+}
---
+++ src/main/java/pl/springbootswitchlanguage/SpringConfiguration.java
@@ -0,0 +1,37 @@
+package pl.springbootswitchlanguage;
+
+import java.util.Locale;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+@Configuration
+@ComponentScan(basePackages = "com.baeldung.internationalization.config")
+public class SpringConfiguration implements WebMvcConfigurer {
+
+  @Bean
+  public LocaleResolver localeResolver() {
+    Locale locale = new Locale("pl", "PL");
+    SessionLocaleResolver slr = new SessionLocaleResolver();
+    slr.setDefaultLocale(locale);
+    return slr;
+  }
+
+  @Bean
+  public LocaleChangeInterceptor localeChangeInterceptor() {
+    LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+    lci.setParamName("lang");
+    return lci;
+  }
+
+  @Override
+  public void addInterceptors(InterceptorRegistry registry) {
+    registry.addInterceptor(localeChangeInterceptor());
+  }
+}
