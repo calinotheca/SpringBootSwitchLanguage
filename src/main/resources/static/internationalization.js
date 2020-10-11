254406353b2a5b313d8cd4efc8a9f32a810385be
kwasc <kwasc@LAPTOP-2JHTQSFL>
2020-10-11 22:24:12 +0200

    v.2

--- src/main/resources/static/internationalization.js
+++ src/main/resources/static/internationalization.js
@@ -1,10 +1,19 @@
-$(document).ready(function() {
-    $("#locales").change(function () {
-        var selectedOption = $('#locales').val();
-        if (selectedOption != ''){
-            window.location.replace('?lang=' + selectedOption);
-        }
-    });
-});/**
+/**
  * 
  */
+
+$(document).ready(function() {
+	$("#locales").change(function() {
+		var selectedOption = $('#locales').val();
+		if (selectedOption != '') {
+
+			$.ajax({
+				url: "?lang=" + selectedOption,
+				context: document.body
+			}).done(function() {
+				location.reload();
+			});
+
+		}
+	});
+});
